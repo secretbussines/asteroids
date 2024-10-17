73945542c95d5dd70f2170c12b68f00d6df5f881
secretbussines <fudaakbar@gmail.com>
2024-10-17 11:13:11 +0500

    adding movement and rotation to the player

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ def main():
     dt = 0
 
     player =  Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    while True:
+
+    # Game loop
+    while True:    
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
+        player.update(dt)
+        
         screen.fill("black")
         player.draw(screen)
-
-
         pygame.display.flip()
+
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
